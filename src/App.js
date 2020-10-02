e9e9004260ba848b5591098df93b1079299190a5
Ken Ashley <ken.ashley97@yahoo.com>
2020-10-01 22:14:03 -0700

    Created temporary app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/home";
 import Chess from "./components/chess/chess";
 import CVMaze from "./components/cvmaze/cvmaze";
 import DejaPhoto from "./components/dejaphoto/dejaphoto";
+import TempApp from "./components/tempApp/tempApp";
 import NotFound from "./components/not-found/NotFound";
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <Route exact path="/chess" component={Chess} />
         <Route exact path="/cvmaze" component={CVMaze} />
         <Route exact path="/dejaphoto" component={DejaPhoto} />
+        <Route exact path="/tempApp" component={TempApp} />
         <Route path="/" component={NotFound} />
       </Switch>
     );
