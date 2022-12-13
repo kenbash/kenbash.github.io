8d2363e7e2980398214a106a7f7c7791138f8d0a
Ken Ashley <ken.ashley97@yahoo.com>
2022-12-12 21:47:35 -0800

    Minor performance improvement

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 
 window.onload = () => {
   const header = document.getElementById('header-fill');
-  let opacity = 0, animTriggered = false;
-
-  function setHeaderOpacity() {
-    opacity = window.scrollY / (screen.height / 2);
-    header.style.opacity = Math.min(opacity, 1);
-  }
+  const img1 = document.getElementById('img-1');
+  const img2 = document.getElementById('img-2');
+  const img3 = document.getElementById('img-3');
+  const footerText = document.getElementById('footer-text');
+  let opacity = 0, didScroll = true;
 
   function onScroll() {
-    if (animTriggered) return;
-    if (window.scrollY > (screen.height / 2)) {
-      document.getElementById('img-1').classList.add('glow');
-      document.getElementById('img-2').classList.add('glow');
-      document.getElementById('img-3').classList.add('glow');
-      document.getElementById('footer-text').classList.add('slide-up');
-      animTriggered = true;
-    }
+    opacity = window.scrollY / (screen.height / 2);
+    header.style.opacity = Math.min(opacity, 1);
+    didScroll = true;
   }
 
-  window.addEventListener('scroll', setHeaderOpacity);
+  const scrollTrigger = setInterval(() => {
+    if (didScroll && window.scrollY > screen.height / 2) {
+      img1.className = 'glow';
+      img2.className = 'glow';
+      img3.className = 'glow';
+      footerText.className = 'slide-up';
+      clearInterval(scrollTrigger);
+    }
+    didScroll = false;
+  }, 100);
+
   window.addEventListener('scroll', onScroll);
 };
