a3b84723b33e069ee12d2426cafed7b4e7f9141b
Ken Ashley <ken.ashley97@yahoo.com>
2022-12-12 21:28:13 -0800

    Added animations and improved text size

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 
 window.onload = () => {
   const header = document.getElementById('header-fill');
-  let opacity;
+  let opacity = 0, animTriggered = false;
 
   function setHeaderOpacity() {
     opacity = window.scrollY / (screen.height / 2);
     header.style.opacity = Math.min(opacity, 1);
   }
 
+  function onScroll() {
+    if (animTriggered) return;
+    if (window.scrollY > (screen.height / 2)) {
+      document.getElementById('img-1').classList.add('glow');
+      document.getElementById('img-2').classList.add('glow');
+      document.getElementById('img-3').classList.add('glow');
+      document.getElementById('footer-text').classList.add('slide-up');
+      animTriggered = true;
+    }
+  }
+
   window.addEventListener('scroll', setHeaderOpacity);
+  window.addEventListener('scroll', onScroll);
 };
