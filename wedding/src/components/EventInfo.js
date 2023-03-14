498819f8c1081096d1d78defab2a91aad3e8a735
Ken Ashley <ken.ashley97@yahoo.com>
2023-03-13 17:23:04 -0700

    Minor fixes

--- wedding/src/components/EventInfo.js
+++ wedding/src/components/EventInfo.js
@@ -50,18 +50,18 @@ function EventInfo(props) {
             <p>Dinner, Drinks, and Dancing!</p>
           </div>
         </div>
-      </div>
-      <div className="detail-location">
-        <iframe
-          title="Edgewood Tahoe location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.133859461303!2d-119.9511721843823!3d38.9666113504658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80999a9a74643ee3%3A0x8982b322198f6521!2sEdgewood%20Tahoe!5e0!3m2!1sen!2sus!4v1678581809673!5m2!1sen!2sus"
-          width="500"
-          height="400"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
+        <div className="detail-location">
+          <iframe
+            title="Edgewood Tahoe location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.133859461303!2d-119.9511721843823!3d38.9666113504658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80999a9a74643ee3%3A0x8982b322198f6521!2sEdgewood%20Tahoe!5e0!3m2!1sen!2sus!4v1678581809673!5m2!1sen!2sus"
+            width="500"
+            height="400"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
       </div>
     </div>
   );
