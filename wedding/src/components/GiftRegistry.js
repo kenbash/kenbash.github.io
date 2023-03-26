b69c709c0cadca6e55671fa61496cdd12b213eef
Ken Ashley <ken.ashley97@yahoo.com>
2023-03-26 15:23:26 -0700

    Added registry embed

--- wedding/src/components/GiftRegistry.js
+++ wedding/src/components/GiftRegistry.js
@@ -19,17 +19,35 @@ function GiftRegistry(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [contentRef]);
 
-  // useEffect?
-  // const registryEmbed =
-  //   '<a class="zola-registry-embed" href="https://www.zola.com/registry/alyssaandkenny2023" data-registry-key="alyssaandkenny2023">Our Zola Wedding Registry</a><script>!function(e,t,n){var s,a=e.getElementsByTagName(t)[0];e.getElementById(n)||(s=e.createElement(t),s.id=n,s.async=!0,s.src="https://widget.zola.com/js/widget.js",a.parentNode.insertBefore(s,a))}(document,"script","zola-wjs");</script>';
+  useEffect(() => {
+    const source = document.getElementById('zola-wjs');
+    if (source) source.parentElement.removeChild(source);
+
+    const a = document.getElementsByTagName('script')[0];
+    const s = document.createElement('script');
+    s.id = 'zola-wjs';
+    s.async = true;
+    s.src = 'https://widget.zola.com/js/widget.js';
+    a.parentNode.insertBefore(s, a);
+  }, []);
 
   return (
     <div className="gift-registry">
       <div className="title">
         <h1>Gift Registry</h1>
       </div>
-      <div className="card" ref={contentRef}>
-        <p style={{ textAlign: 'center' }}>Coming soon!</p>
+      <div className="card" ref={contentRef} style={{ textAlign: 'center' }}>
+        <div className="sub-title">
+          <p>Your presence is present enough!</p>
+          <p className="sub">but if you insist...</p>
+        </div>
+        <a
+          className="zola-registry-embed"
+          href="https://www.zola.com/registry/alyssaandkenny2023"
+          data-registry-key="alyssaandkenny2023"
+        >
+          Our Wedding Registry
+        </a>
       </div>
     </div>
   );
